df83beebd979daf6fa8143e1151995b4c878b135
lebarsfa <fabrice.le_bars@ensta-bretagne.fr>
2016-05-06 19:58:10 +0200

    Updated Visual Studio unistd.h

--- To add or modify if needed/VS/unistd.h
+++ To add or modify if needed/VS/unistd.h
@@ -9,7 +9,7 @@
 
 /* 
 This file intended to serve as a drop-in replacement for unistd.h on Windows. 
-Please add functionality as neeeded.
+Please add/remove functionality as neeeded.
 
 See http://pubs.opengroup.org/onlinepubs/7908799/xsh/unistd.h.html.
 */ 
@@ -18,6 +18,8 @@ See http://pubs.opengroup.org/onlinepubs/7908799/xsh/unistd.h.html.
 #include <direct.h>
 #include <process.h>
 //#include <getopt.h> // getopt from: http://www.pwilson.net/sample.html.
+// To get Sleep().
+#include <winsock2.h>
 
 //#define srandom srand 
 //#define random rand 
@@ -29,6 +31,12 @@ See http://pubs.opengroup.org/onlinepubs/7908799/xsh/unistd.h.html.
 //#define ftruncate _chsize 
 
 //#define ssize_t int 
+
+__inline int usleep(unsigned int usec)
+{
+	Sleep(usec/1000);
+	return 0;
+}
 
 #endif // UNISTD_H
 
